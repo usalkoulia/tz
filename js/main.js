e3d46d731f0128d0fbbb7daa86641b2bde6a3e10
Ulia Usalko <usalko.ulia@gmail.com>
2019-03-17 23:28:13 +0200

    finish

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var contact_button = document.querySelector("#contact-button");
+var contact_form = document.querySelector("#contact-form");
+var close_contact_button = document.querySelector("#close-contact-button");
+
+contact_button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  contact_form.classList.add("backdrop--active");
+})
+
+close_contact_button.addEventListener("click", function() {
+  contact_form.classList.remove("backdrop--active");
+})
+
+var user_button = document.querySelector("#user-button");
+var user_form = document.querySelector("#user-form");
+var close_user_button = document.querySelector("#close-user-button");
+
+user_button.addEventListener("click", function() {
+  user_form.classList.add("backdrop--active");
+})
+
+close_user_button.addEventListener("click", function() {
+  user_form.classList.remove("backdrop--active");
+})
